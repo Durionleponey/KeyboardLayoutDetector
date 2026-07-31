9646eeb2b9b0fce019dcaa892c9e1f90407f34c1
robin <robin.clarembaux@gmail.com>
2026-07-31 17:32:28 +0200

    requirement

--- code/main.py
+++ code/main.py
@@ -4,6 +4,9 @@ import skimage
 # from matplotlib import pyplot as plt # Plus nécessaire
 from extraction import selectionner_images
 # from trie import detect_layout_from_image # Plus nécessaire ici
+import os
+os.environ["TCL_LIBRARY"] = r"C:\Program Files\Python313\tcl\tcl8.6"
+os.environ["TK_LIBRARY"]  = r"C:\Program Files\Python313\tcl\tk8.6"
 
 def main():
     print("--- Programme Principal ---")
