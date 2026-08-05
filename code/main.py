d41eb7b0532215dba18ca5e5422bfd7213b90255
robin <robin.clarembaux@gmail.com>
2026-08-05 16:00:29 +0200

    refactoring loading screen

--- code/main.py
+++ code/main.py
@@ -2,6 +2,8 @@
 import threading
 from tkinter import Image, PhotoImage, Label
 
+from utils import loading_screen
+
 import customtkinter as ctk
 
 import os
@@ -16,35 +18,12 @@ importTemp =None
 
 def main():
 
-    loadingScreen = ctk.CTk()
-    loadingScreen.title("KeyboardLayoutIdentificator")
-    loadingScreen.geometry("1000x700")
-    loadingScreen.resizable(False, False)\
-
-    photo = PhotoImage(file="./asset/keyboardLayoutDetectorLogo.png")
-    lbl = Label(loadingScreen, image=photo)
-    lbl.place(relx=0.5, rely=0.4, anchor="center")
-
-    ctk.CTkLabel(loadingScreen, text="Loading Keyboard Layout Identificator...", font=("Roboto", 18)).place(relx=0.5, rely=0.75, anchor="center")
-    bar = ctk.CTkProgressBar(loadingScreen, mode="indeterminate", width=500)
-    bar.place(relx=0.5, rely=0.8, anchor="center")
-    bar.start()
-
     def load():
         global importTemp
         from extraction import selectionner_images as f
         importTemp = f
-        loadingScreen.after(0, kill_clean)
 
-    def kill_clean():
-        bar.stop()
-        loadingScreen.quit()
-
-
-
-    threading.Thread(target=load, daemon=True).start()
-    loadingScreen.mainloop()
-    loadingScreen.destroy()
+    loading_screen(load)
 
     importTemp()
 
--- code/utils.py
+++ code/utils.py
@@ -1,6 +1,10 @@
+import threading
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from tkinter import PhotoImage, Label
+import customtkinter as ctk
 
 
 def upscale_image(img, scale=2, interpolation=cv2.INTER_CUBIC):
@@ -134,3 +138,30 @@ def draw_boxes_on_image(img, detections):
         )
 
     return img_display
+
+
+
+def loading_screen(load):
+
+    loadingScreen = ctk.CTk()
+    loadingScreen.title("KeyboardLayoutIdentificator")
+    loadingScreen.geometry("1000x700")
+    loadingScreen.resizable(False, False)\
+
+    photo = PhotoImage(file="./asset/keyboardLayoutDetectorLogo.png")
+    lbl = Label(loadingScreen, image=photo)
+    lbl.place(relx=0.5, rely=0.4, anchor="center")
+
+    ctk.CTkLabel(loadingScreen, text="Loading Keyboard Layout Identificator...", font=("Roboto", 18)).place(relx=0.5, rely=0.75, anchor="center")
+    bar = ctk.CTkProgressBar(loadingScreen, mode="indeterminate", width=500)
+    bar.place(relx=0.5, rely=0.8, anchor="center")
+    bar.start()
+
+    def load2():
+        load()
+        loadingScreen.after(0, loadingScreen.quit)
+
+
+    threading.Thread(target=load2, daemon=True).start()
+    loadingScreen.mainloop()
+    loadingScreen.destroy()
